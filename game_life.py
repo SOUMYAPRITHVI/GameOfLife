b51c5b421495ad1615fdd1747d38503ec845b3f4
soumya <soumyakn89@gmail.com>
2023-11-08 12:19:09 +0530

    Green:Add update grid function for no alive neighbors

--- game_life.py
+++ game_life.py
@@ -8,3 +8,22 @@ def create_initial_grid(GRID_WIDTH=60,GRID_HEIGHT=60):
         row = [random.randint(0, 0) for _ in range(GRID_HEIGHT)]
         grid.append(row)
     return grid
+
+def update_grid(grid,x,y):
+    new_grid = create_initial_grid(x,y)
+    for i in range(x):
+        for j in range(y):
+            live_neighbors=0
+            for dx in [-1,0,1]:
+                for dy in [-1,0,1]:
+                    if dx==0 and dy==0:
+                        continue
+                    new_x,new_y=i+dx,j+dy
+                    if 0<=new_x<y and 0<=new_y<x:
+                        live_neighbors+=grid[new_x][new_y]   
+            if grid[i][j] == 1:
+                if live_neighbors < 1:
+                    new_grid[i][ j] = 0
+                else:
+                    new_grid[i][j] = 1               
+    return new_grid

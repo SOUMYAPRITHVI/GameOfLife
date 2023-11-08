2a333e2e0019e30771c6f57a9edb823319c3e7b4
soumya <soumyakn89@gmail.com>
2023-11-08 12:17:22 +0530

    Red:Add testcase for no alive neighbors

--- test_game.py
+++ test_game.py
@@ -16,5 +16,13 @@ def test_simulate_erase():
     grid[2][3]=0
     assert grid==[[0, 0, 0, 0, 0], [0, 0, 0, 0, 1], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 
-
+def test_update_no_live_neighbor():
+    grid=game_life.create_initial_grid(5,5)
+    grid[1][1]=1
+    grid[1][4]=1
+    grid[3][1]=1
+    grid[3][4]=1
+    new_grid=game_life.update_grid(grid,5,5)
+    assert new_grid==[[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+    
   
